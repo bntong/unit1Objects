8e5f18faf05b81c7c7816c8b4762a3cdfcfa888c
bntong <bntong@NN123-HA-06.HS.sd203.org>
2015-09-02 15:08:24 -0500

    Fixed turtle stuff
    Did more turtle stuff

--- Chapter 2 Projects/DieSimulator.java
+++ Chapter 2 Projects/DieSimulator.java
@@ -3,14 +3,15 @@ import java.util.Random;
 public class DieSimulator
 {
     public static void main(String[] args)
-    {
-        int x = 0;
-        while(x < 1000)
-        {
+    {  
+            // Option A
             Random random = new Random();
             System.out.println(random.nextInt(6)+1);
-            x += 1;
-        }
             
+            // Option B
+            double random2 = Math.random(); // returns [0,1)
+            random2 *=6;    //returns [0, 6)
+            random2 += 1;    //returns [1, 7)
+            System.out.println( (int) random2);
     }
 }
--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -9,19 +9,35 @@ public class TurtleWorld
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         Turtle turtle2 = new Turtle(turtleWorld);
+        turtle2.forward(50);
         turtle2.setPenColor(Color.GREEN);
         turtle2.penDown();
-        while (true) {
+        for (int i = 0; i < 50; i ++) 
+        {
             turtle.turn(60);
             turtle.forward(100);
             turtle.turn(120);
             turtle.forward(100);
             turtle.turn(120);
             turtle.forward(100);
-            turtle.turn(100);
+            turtle.turn(50);
         }
+        for (int i = 0; i < 50; i ++) 
+        {
+            turtle2.turn(60);
+            turtle2.forward(100);
+            turtle2.turn(120);
+            turtle2.forward(100);
+            turtle2.turn(120);
+            turtle2.forward(100);
+            turtle2.turn(50);
+        }
+        
+        
+        
     }
+}
     
          
-    }
+    
 
